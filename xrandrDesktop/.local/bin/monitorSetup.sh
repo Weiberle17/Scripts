88326ef880ced6a5e98e3769c9a78079da998333
Weiberle17 <maxweiberle63@gmail.com>
2023-03-02 23:27:34 +0100

    added awesome reload

--- xrandrDesktop/.local/bin/monitorSetup.sh
+++ xrandrDesktop/.local/bin/monitorSetup.sh
@@ -4,3 +4,4 @@ xrandr --output DisplayPort-1 --primary --auto
 echo "First Screen configured"
 xrandr --output DisplayPort-2 --right-of DisplayPort-1 --rotate right --auto
 echo "Second Screen configured"
+echo 'awesome.restart()' | awesome-client
