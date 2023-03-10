89b6bbcea0671acc295515026124cfc371f03631
Weiberle17 <maxweiberle63@gmail.com>
2023-03-10 11:40:16 +0100

    Added flexiblity
    Added option to change timer between notifications. Default Value stays
    at 15 minutes

--- drinkWater/.local/bin/waterTimer.sh
+++ drinkWater/.local/bin/waterTimer.sh
@@ -1,8 +1,10 @@
-waterReminder () {
-  notify-send -i ~/.local/bin/waterIcon.png "Time to drink Water!!";
-}
-
 while true; do
-  waterReminder;
-  sleep 9000;
+  notify-send -i ~/.local/bin/waterIcon.png "Time to drink Water!!";
+  if [[ $1 ]]; then
+    echo "The Timer was set to $(($1/60)) min"
+    sleep $1;
+  else
+    echo "No Timer set, using default 15 min";
+    sleep 9000;
+  fi
 done
